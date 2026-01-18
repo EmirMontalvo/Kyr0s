9232ce78842f5890347adf8549b2cbb47434459a
Emir Montalvo <emoncan27@gmail.com>
2026-01-18 02:44:57 -0600

    Remove all content from repository

--- OneDrive/Desktop/kyros-barber-web propio/kyros-barber-web propia/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://qyyhembukflbxjbctuav.supabase.co',
-  supabaseKey: 'sb_publishable_iyXy7g8HFkSWS-hscoR4HQ_6ONq_SzF'
-};
-
-
--- OneDrive/Desktop/mi_septima_chamba1.1/mi_septima_chamba/android/app/src/main/kotlin/com/example/mi_septima_chamba/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mi_septima_chamba
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
